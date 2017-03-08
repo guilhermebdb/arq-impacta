9a2200cd58ae3ea5373426e4b6a4634539836f4c
guilhermebdb <guilhermeedaniel@gmail.com>
2017-03-07 22:06:46 -0300

    Add files via upload

---
+++ SaudacaoCliente.java
@@ -0,0 +1,27 @@
+package br.edu.impacta.arq4;
+
+import static javax.swing.JOptionPane.*;
+
+import java.io.*;
+import java.net.*;
+
+public class SaudacaoCliente {
+	
+	public static void main (String [] args) throws UnknownHostException, IOException {
+		String nome = showInputDialog("Qual é o seu nome?");
+		Socket soc = new Socket("127.0.0.1", 58888);
+		InputStream is = soc.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		OutputStream os = soc.getOutputStream();
+		PrintWriter pw = new PrintWriter(os);
+		pw.println(nome);
+		pw.flush();
+		String resposta = br.readLine();
+		showInputDialog(null, "Resposta Servidor: " + resposta);
+		pw.close();
+		br.close();
+		soc.close();
+					
+	}
+
+}
